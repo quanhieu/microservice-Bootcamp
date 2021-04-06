a346574301690663fdf95da88fb8c7d8e8891d7d
quanghieu <truongquanghieu8198@gmail.com>
2021-04-06 09:50:33 +0700

    build Create Posts and handle CORS errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+import React from 'react';
+import PostCreate from './PostCreate';
+
 export default function App() {
   return (
-    <div>Blog app</div>
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
   );
 }
 
